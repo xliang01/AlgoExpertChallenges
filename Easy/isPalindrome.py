eac2fc7b943274d3a92860db504f1f3efcc7491d
Xiao Liang <xliang01@gmail.com>
2019-09-03 00:03:13 -0700

    Palindrome

---
+++ Easy/isPalindrome.py
@@ -0,0 +1,16 @@
+def isPalindrome(string):
+    start = 0
+    end = len(string) - 1
+
+    while start < end:
+        if string[start] != string[end]:
+            return False
+        start += 1
+        end -= 1
+
+    return True
+
+print(isPalindrome(""))
+print(isPalindrome("a"))
+print(isPalindrome("abc"))
+print(isPalindrome("aba"))
